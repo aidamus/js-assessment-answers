4354f713a73ba52eb6c3fd62d7bab9e8abc45852
Eric Terpstra <eterps@gmail.com>
2012-05-06 17:29:14 -0500

    done with functions

--- app/functions.js
+++ app/functions.js
@@ -10,30 +10,54 @@ define(function() {
         return fn.apply(obj);
     },
 
-    functionFunction : function(arg1) {
+    functionFunction : function() {
+        var arg1 = arguments[0];
         return function (arg2) {
             return arg1 + ', ' + arg2;
         }
     },
 
     partial : function() {
-        
+        var fn = arguments[0];
+        var args = Array.prototype.slice.call( arguments, 1 );
+        return function (punctuation) {
+            return fn(args[0],args[1],punctuation);
+        };
     },
 
     useArguments : function() {
-
+        var argumentSum = 0;
+        for ( var i=0; i < arguments.length; i++ ) {
+            argumentSum += arguments[i];
+        }
+        return argumentSum;
     },
 
     callIt : function() {
-
+        var fn = arguments[0];
+        var args = Array.prototype.slice.call( arguments, 1 );
+        return fn.apply(null,args);
     },
 
     curryIt : function() {
-
+        var fn = arguments[0];
+        var args = Array.prototype.slice.call( arguments, 1 );
+        return function() {
+            var newArgs = Array.prototype.slice.call( arguments );
+            var allArgs = args.concat(newArgs);
+            return fn.apply(null, allArgs);
+        };        
     },
 
     makeClosures : function() {
-
+        var arr = arguments[0];
+        var funcArray = [];
+        arr.forEach(function (el) {
+           funcArray.push(function () {
+               return el*el;
+           });
+        });
+        return funcArray;
     }
   };
 });
